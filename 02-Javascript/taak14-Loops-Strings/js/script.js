c55a1db93835fabbe02c93f92994185972060439
JuliandeVries <Julian.devries@student.rocva.nl>
2021-09-16 10:53:38 +0200

    huts

--- 02-Javascript/taak14-Loops-Strings/js/script.js
+++ 02-Javascript/taak14-Loops-Strings/js/script.js
@@ -7,3 +7,11 @@ function outputLetters(str){
 
 var voorbeeldString = "Dit is een array van letters";
 console.log(voorbeeldString.length); // geeft terug: 
+  
+a = 0, b = 1, f = 1;
+for(i = 1; i < 100; i++) {
+    f = a + b;
+    a = b;
+    b = f;
+    console.log(f)
+}
